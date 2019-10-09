faf0d3b4587399a0b4f4ee5da0fc1dd7b4a275b1
Amen <Amen@DESKTOP-4K4949S>
2019-10-09 17:48:01 +0900

    191009 17:47 설마

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.stereotype.*;
 
-@Controller
 @EnableJpaAuditing	// JPA Auditing 활성화
 @SpringBootApplication
 public class DemoApplication {
@@ -21,7 +18,7 @@ public class DemoApplication {
 	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "app/config/springboot-calendar/real-application.yml";
+			+ "/app/config/springboot-calendar/real-application.yml";
 	
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(DemoApplication.class)
